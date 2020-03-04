2fff7091acca3584389e95a5f91d1f0368bc9d86
Ghabriellycosta <60987490+Ghabriellycosta@users.noreply.github.com>
2020-03-04 18:03:35 -0400

    Add files via upload

---
+++ LabelFrame/src/LabelTest.java
@@ -0,0 +1,22 @@
+
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+    
+   public static void main (String [] args){
+       
+       LabelFrame labelFrame = new LabelFrame();
+       labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+       labelFrame.setSize(506,900);
+       labelFrame.setVisible(true);
+   } 
+    
+    
+    
+    
+    
+    
+    
+    
+}
