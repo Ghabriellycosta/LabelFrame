e752cd28ba206ef01487aca03958370bc3a00d00
Ghabriellycosta <60987490+Ghabriellycosta@users.noreply.github.com>
2020-03-11 18:48:19 -0400

    Add files via upload

---
+++ TextFieldFrame/src/textfieldframe/TextFieldFrame.java
@@ -0,0 +1,72 @@
+
+package textfieldframe;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+                        
+
+public class TextFieldFrame extends JFrame {
+    
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPasswordField passwordField;
+    
+    public TextFieldFrame (){
+        
+        super(" Testing JTextField and JPasswordField ");
+        setLayout( new FlowLayout());
+        
+        textField1 = new JTextField(10);
+        add(textField1);
+        
+        textField2 = new JTextField(" escreva um texto aqui");
+        add(textField2);
+        
+        textField3 = new JTextField(" campo de texto não editável",21);
+        textField3.setEditable(false);
+        add (textField3);
+        
+        passwordField = new JPasswordField (" texto escondido");
+        add (passwordField);
+        
+        TextFieldHandler handler = new TextFieldHandler();
+        textField1.addActionListener(handler);
+        textField2.addActionListener(handler);
+        textField3.addActionListener(handler);
+        passwordField.addActionListener(handler);
+    }
+        public class TextFieldHandler implements ActionListener {
+            
+            public void actionPerformed(ActionEvent event){
+                
+                String string = "";
+                if (event.getSource()== textField1)
+                    string = String.format(" textField: %s",event.getActionCommand());
+                else if (event.getSource()== textField2)
+                    string = String.format(" textField2 : %s", event.getActionCommand());
+                else if (event.getSource()== textField3)
+                    string = String.format("textField3 : %s", event.getActionCommand());
+                else if (event.getSource() == passwordField)
+                    string = String.format(" passwordField: %s",event.getActionCommand());
+                
+                JOptionPane.showMessageDialog( null, string);
+                    
+                    
+                    
+            }
+        }   
+        
+    }
+    
+    
+
+
+    
+
